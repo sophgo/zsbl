23557c2ccc768cd638470b7557320ae906b3b55a
haijiao.liu <haijiao.liu@sophgo.com>
2024-05-27 11:16:21 +0800

    iommu: add ssperi subsystem device id config
    Signed-off-by: haijiao.liu <haijiao.liu@sophgo.com>

--- plat/sg2380/include/iommu.h
+++ plat/sg2380/include/iommu.h
@@ -7,8 +7,41 @@
 
 #include <lib/utils_def.h>
 
-#define IOMMU_GLOBAL_CFG        0x2000 + 0x0
-#define BYPASS_ENABLE_OFFSET     BIT(0)
+/* iommu wrapper register */
+#define IOMMU_GLOBAL_CFG	0x2000 + 0x0
+#define BYPASS_ENABLE		BIT(0)
+
+/* ssperi sys top register */
+#define ETH01_AW_DEVID_REG	0x164
+#define ETH23_AW_DEVID_REG	0x168
+#define ETH45_AW_DEVID_REG	0x16c
+#define SATA_CDMA_AW_DEVID_REG	0x170
+
+#define ETH01_AR_DEVID_REG	0x174
+#define ETH23_AR_DEVID_REG	0x178
+#define ETH45_AR_DEVID_REG	0x17c
+#define SATA_CDMA_AR_DEVID_REG	0x180
+
+#define ETH0_SHIFT	0
+#define ETH1_SHIFT	16
+#define ETH2_SHIFT	0
+#define ETH3_SHIFT	16
+#define ETH4_SHIFT	0
+#define ETH5_SHIFT	16
+#define SATA_SHIFT	0
+#define CDMA_SHIFT	16
+
+#define DEVID_MASK	0xFFFF
+
+/* device id */
+#define ETH0_DEVID	1
+#define ETH1_DEVID	2
+#define ETH2_DEVID	3
+#define ETH3_DEVID	4
+#define ETH4_DEVID	5
+#define ETH5_DEVID	6
+#define SATA_DEVID	7
+#define CDMA_DEVID	8
 
 void sg2380_iommu_init(void);
 
