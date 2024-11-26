56bb1656195f5753ce33bb16df62276719097814
Chao Wei <chao.wei@sophgo.com>
2024-11-26 20:06:36 +0800

    SG2042: Fixbug, wrong dtb used
    1. Setup dtb names after config init
    2. Add prefix "riscv64" in sgfat lib
    3. Setup boot device priority before loading any files

--- plat/sg2042/config.c
+++ plat/sg2042/config.c
@@ -51,7 +51,7 @@ int parse_config_file(struct config *cfg)
 	const char *file = "conf.ini";
 
 	size = bdm_get_file_size(file);
-	if (size < 0) {
+	if (size <= 0) {
 		pr_debug("No config file found, using default configurations\n");
 		return -ENOENT;
 	}
