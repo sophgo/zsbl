a82264d8946a5162f5255e85012363e3846c71f7
Chao Wei <chao.wei@sophgo.com>
2024-12-27 17:07:39 +0800

    add alias property in device

--- drivers/base/device.c
+++ drivers/base/device.c
@@ -6,68 +6,28 @@
 #include <driver/device.h>
 #include <lib/container_of.h>
 
-static LIST_HEAD(device_list);
-
-int device_list_devices(void)
-{
-	struct list_head *p;
-	struct device *dev;
-	int i;
-
-	i = 0;
-	list_for_each(p, &device_list) {
-		dev = container_of(p, struct device, list_head);
-		pr_info("%d: %s\n", i, dev->name);
-		++i;
-	}
-
-	return i;
-}
-
-struct device *device_alloc(void)
-{
-	struct device *dev;
-
-	dev = malloc(sizeof(struct device));
-
-	if (dev)
-		memset(dev, 0, sizeof(struct device));
-
-	return dev;
-}
-
-void device_free(struct device *dev)
-{
-	free(dev);
-}
-
-void device_add(struct device *dev)
-{
-	list_add_tail(&dev->list_head, &device_list);
-}
-
-void device_remove(struct device *dev)
-{
-	list_del(&dev->list_head);
-}
-
-int device_count(void)
-{
-	return list_count_nodes(&device_list);
-}
-
-struct device *device_find_by_name(const char *name)
+struct device *device_find_by_name(struct list_head *device_list, const char *name)
 {
 	struct device *dev;
 	struct list_head *p;
 
-	list_for_each(p, &device_list) {
+	list_for_each(p, device_list) {
 		dev = container_of(p, struct device, list_head);
-		if (strcmp(dev->name, name) == 0) {
+		if (strcmp(dev->name, name) == 0)
 			return dev;
-		}
+		if (strcmp(dev->alias, name) == 0)
+			return dev;
 	}
 
 	return NULL;
 }
 
+void device_set_child_name(struct device *child, struct device *parent, const char *sys)
+{
+	int err;
+	err = snprintf(child->name, sizeof(child->name), "%s:%s", sys, parent->name);
+	if (err == sizeof(child->name))
+		pr_warn("Truncated device names\n");
+
+	strcpy(child->alias, parent->alias);
+}
--- include/driver/device.h
+++ include/driver/device.h
@@ -8,6 +8,10 @@
 struct device {
 	struct list_head list_head;
 	char name[DEVICE_NAME_MAX];
+	char alias[DEVICE_NAME_MAX];
 };
+
+struct device *device_find_by_name(struct list_head *device_list, const char *name);
+void device_set_child_name(struct device *child, struct device *parent, const char *sys);
 
 #endif
