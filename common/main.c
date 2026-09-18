00fc49c55624956c0c33db7f4cf5bdeee3e161f9
Chao Wei <chao.wei@sophgo.com>
2026-09-18 10:34:38 +0800

    v3.2.0 release
    Signed-off-by: Chao Wei <chao.wei@sophgo.com>

--- common/main.c
+++ common/main.c
@@ -14,7 +14,7 @@ int plat_main(void);
 /* major, minor, patch */
 
 #define VERSION_MAJOR "3"
-#define VERSION_MINOR "1"
+#define VERSION_MINOR "2"
 #define VERSION_PATCH "0"
 
 #define ZSBL_VERSION	VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH
